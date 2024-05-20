b3425f5f55c1a895ec3cfdb2b930e926b4b4e0af
oferpop <oferpop@gmail.com>
2024-05-20 10:09:08 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+
+from helper.calc import div, maxnumber, mult,sum,div2
+from icecream import ic
+
+
+     
+if __name__=="__main__" :
+    ic( sum(1,2,3))   
+    ic(div(1,2,3))
+    ic(mult(1,2,3))
+    ic(maxnumber(1,2,3))
+    ic(div2(1,2)) 
+    
---
+++ helper/calc.py
@@ -0,0 +1,10 @@
+def div(num1,n2,n3) :
+    return(num1 / n2 /n3)
+def mult (num1,n2,n3):
+    return (num1* n2*n3) 
+def maxnumber(num1, num2, num3):
+    return max(num1, num2, num3) 
+def sum (num1,n2,n3):
+    return (num1 + n2 +n3) 
+def div2(num1,n2,) :
+    return(num1 / n2 )          
